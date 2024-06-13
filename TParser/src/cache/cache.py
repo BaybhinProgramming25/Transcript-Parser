12ad168442237ee117b5e97f104dcc7bd8c40ed2
Baybhin Gurung <baybhin@gmail.com>
2024-06-12 21:34:52 -0400

    major changes

--- TParser/src/cache/cache.py
+++
@@ -1,2 +0,0 @@
-# We will most likely use Redis for our cache and it can also suit some of our other needs 
-
--- script.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Build the Docker image
-docker build -t my-nginx-parser .
-
-# Run the Docker container
-docker run  -d -p 8080:80 my-nginx-parser
